49caaf85382451f8fd8abc7d5a4e6bf6a1bb5503
Arelia Maia Ashary <maiaarelia@gmail.com>
2023-09-11 01:47:59 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas_kalkulator_fix/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tugas_kalkulator_fix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pertemuan4_constraintlayout.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
